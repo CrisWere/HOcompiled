9309bcecb889d559d1eb88d175ec57decc216e49
CrisWere <cristina.werenitzky@gmail.com>
2017-03-04 19:39:08 -0300

    Agrego archivo reglas.txt y archivos generados en el ejercicio

--- multilenguaje/04-c-from-f/f-main.f90
+++ multilenguaje/04-c-from-f/f-main.f90
@@ -1,13 +1,28 @@
+
 program testsum
   implicit none
 
   integer, parameter :: n=200
   integer :: data(n), asum, i
 
+ interface
+    subroutine sum_abs_( arre, num , res) bind ( c )
+      use iso_c_binding
+      integer ( c_int) :: arre(*)
+      integer ( c_int ) :: num
+      integer ( c_int ) :: res
+    end subroutine sum_abs_
+  end interface
+
   do i=1,200
-    data(i) = i-100
+     data(i) = i-100
   end do
 
-  call sum_abs(data,n,asum)
+
+  
+  call sum_abs_(data,n,asum)
+
   print*, 'sum=',asum
 end program testsum 
+
+
